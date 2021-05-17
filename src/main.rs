c5fd2574e0edc51fae1c92a8fe6306324b108191
zUnixorn <jonas.crull@gmx.de>
2021-05-17 16:04:17 +0200

    Added TODOs and began fixing them.

--- src/main.rs
+++ src/main.rs
@@ -12,17 +12,24 @@ use serde_json;
 use std::path::Path;
 use serde::Serialize;
 use serde::Deserialize;
+use xdg;
 //use std::thread;
 //use std::sync::mpsc;
 
+
+//TODO inizlize http_client in main and get rid of the token option
+//TODO make the config file path use the XDG Base directories
+//TODO put the request parts in its own call_backend function
 //TODO rewrite login so it takes email and password as arguments
 
 //TOKEN_FILE location
-static TOKEN_FILE: &'static str = "token.json";
+static TOKEN_FILE: &'static str = "~/.config/yap-frontend-cli/token.json";
 
-struct User {
+
+struct GlobalData {
     http_client: blocking::Client,
     token: Option<String>,
+    //config_home: xdg::BaseDirectories
 }
 
 #[derive(Serialize, Deserialize)]
@@ -41,6 +48,11 @@ enum TokenLoadError {
     TokenExpired,
     FileNotFound,
     FileNotReadable,
+}
+
+enum RequestError {
+    StatusError,
+    RequestFailed,
 }
 
 fn exit(root: &mut Cursive) {
@@ -97,18 +109,6 @@ fn login_page(root: &mut Cursive) {
                 .child(
                     TextView::new(" Remember Me")
                 )
-            /*)
-            .child(
-            LinearLayout::horizontal()
-                .child(
-                    Checkbox::new()
-                        .on_change(|root, state| {
-
-                        })
-                )
-                .child(
-                    TextView::new(" Remember Me")
-                )*/
             )
         )
         .button("Back", |root| welcome_page(root))
@@ -119,19 +119,12 @@ fn login_page(root: &mut Cursive) {
 fn login(root: &mut Cursive) {
 
     //Get HTTP client if it exists else create one and store it for later use
-    let http_client=  root.take_user_data::<User>().unwrap_or(
-        User {
+    let http_client = root.take_user_data::<GlobalData>().unwrap_or(
+        GlobalData {
             http_client: blocking::Client::new(),
             //token: "".to_string(),
             token: None,
         }).http_client;
-
-    /*let email = root.find_name::<EditView>("EMAIL_LOGIN")
-        .unwrap_or(
-            root.find_name::<EditView>("EMAIL_REGISTER")
-                .expect("couldn't find view by name")
-        )
-        .get_content();*/
 
     let email = root.find_name::<EditView>("EMAIL_LOGIN")
         .unwrap_or_else(
@@ -156,16 +149,13 @@ fn login(root: &mut Cursive) {
             email,
             password
         ))
-        .send() { // <- Some error isn't properly caught, to try type invalid info into login form and observe that is still executes the successful branch
+        .send() {
 
-        Ok(request) => {
-            //let mut file = File::create("reached");
-            //file.unwrap().write_all(request.status().as_str().as_bytes()).unwrap();
-
+        Ok(request) => { // <- If the status code is an Error it will still return an Ok()
             if request.status().is_success() {
                 if let Ok(_) = fs::remove_file(TOKEN_FILE) {};
                 //Store the client and token
-                root.set_user_data(User {
+                root.set_user_data(GlobalData {
                     http_client,
                     token: Some(request.text()
                         .expect("request didn't return a token")),
@@ -190,7 +180,7 @@ fn login(root: &mut Cursive) {
                                         .expect("couldn't find view by name")
                                         .get_content()
                                         .to_string(),
-                                    token: root.user_data::<User>()
+                                    token: root.user_data::<GlobalData>()
                                         .expect("no user data set")
                                         .token
                                         .as_ref()
@@ -235,43 +225,6 @@ fn login(root: &mut Cursive) {
     }
 }
 
-/*fn remember_me_login(root: &mut Cursive) {
-    if Path::exists(TOKEN_FILE.as_ref()) { //error is between here ----------------------------------------------------
-        let mut json = String::new();
-        File::open(TOKEN_FILE)
-            .unwrap()
-            .read_to_string(&mut json);
-        let json = serde_json::from_str::<TokenFile>(&*json).unwrap();
-
-        let token = json.token;
-        //let user_mail = json.user_mail;
-
-        if let Ok(status) = root.user_data::<User>()
-            .unwrap()
-            .http_client
-            .get("https://backend.yap.dragoncave.dev/security/token/checkValid")
-            .header("Token", token.clone())
-            .send() {
-            if status.text().unwrap().parse::<bool>().unwrap() {
-                root.with_user_data::<_, User, _>(|user| user.token = token.clone());
-                notify_popup(root, "Success!", "Could import token, noice!"); // <- should eventually be replaced with main_screen(root)
-            } else {
-                fs::remove_file(TOKEN_FILE);
-                root.add_layer(
-                    Dialog::text(
-                        "your token is probably invalid, you should try to reauthenticate"
-                    )
-                        .title("¯\\_(ツ)_/¯")
-                        .button("reauthenticate", |root| {
-                            root.pop_layer();
-                            root.pop_layer();
-                            login_page(root);
-                        })
-                );
-            }
-        }
-    }
-}*/
 
 fn main_screen(root: &mut Cursive) {
     root.pop_layer();
@@ -372,6 +325,27 @@ fn notify_popup(root: &mut Cursive, title: &str, message: &str) {
     );
 }
 
+/*fn call_backend(root: &mut Cursive, end_point: &str, headers: reqwest::header::HeaderMap, body: &str) -> Result<String, RequestError> {
+    let &mut http_client = &root.user_data::<User>().unwrap().http_client;
+
+    let mut request = http_client.post(format!("https://backend.yap.dragoncave.dev/{}", end_point))
+        .body(body)
+        .headers(headers);
+
+    match request.send() {
+        Ok(response) => {
+            if response.status().is_success() {
+                return Ok(response.text().unwrap());
+            } else {
+                return Err(RequestError::StatusError);
+            }
+        },
+        Err(error) => {
+            return Err(RequestError::RequestFailed);
+        }
+    };
+}*/
+
 fn check_register(root: &mut Cursive) -> Result<(), RegisterInvalid> {
     let username = root.find_name::<EditView>("USERNAME_REGISTER")
         .expect("couldn't find view by name");
@@ -407,60 +381,10 @@ fn check_register(root: &mut Cursive) -> Result<(), RegisterInvalid> {
 
         return Err(RegisterInvalid::InvalidPassword);
     }
-
-    /*if let Some(view) = root.find_name::<EditView>("USERNAME_REGISTER") {
-        if view.get_content().as_str().eq("") || view.get_content().len() > 32 {
-            return Err(RegisterInvalid::InvalidUsername);
-        }
-    } else {
-        panic!("couldn't find view by name");
-    }
-
-
-    if let Some(view) = root.find_name::<EditView>("EMAIL_REGISTER") {
-        if view.get_content().as_str().eq("") ||
-            Regex::new(/*<editor-fold desc="email regEx">*/"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
-                       /*</editor-fold>*/).unwrap().is_match(view.get_content().as_str()).not() {
-            return Err(RegisterInvalid::InvalidEmail);
-        }
-    } else {
-        panic!("Couldn't find view by name");
-    }
-
-
-    if let Some(view) = root.find_name::<EditView>("PASSWORD_REGISTER") {
-        if let Some(check_view) = root.find_name::<EditView>("PASSWORD_CHECK_REGISTER") {
-            if view.get_content().len() < 10 ||
-                view.get_content().eq(&check_view.get_content()).not() ||
-                view.get_content().len() > 1024 {
-                return Err(RegisterInvalid::InvalidPassword);
-            }
-        }
-    } else {
-        panic!("Couldn't find view by name");
-    }*/
-
     return Ok(());
 }
 
 fn register(root: &mut Cursive) {
-    /*let username = root.find_name::<EditView>("USERNAME")
-        .unwrap().
-        get_content();
-    let email = root.find_name::<EditView>("EMAIL")
-        .unwrap()
-        .get_content();
-    let password = root.find_name::<EditView>("PASSWORD")
-        .unwrap()
-        .get_content();
-
-    call_api(root, "add_user", format!(
-        "{{\"username\":\"{}\",\"emailAddress\":\"{}\",\"password\":\"{}\"}}",
-        username,
-        email,
-        password
-    ));*/
-
     let http_client = blocking::Client::new();
 
     match http_client.post("https://backend.yap.dragoncave.dev/user")
@@ -479,7 +403,7 @@ fn register(root: &mut Cursive) {
         ))
         .send() {
         Ok(_) => {
-            root.set_user_data(User {
+            root.set_user_data(GlobalData {
                 http_client,
                 //token: "".to_string(),
                 token: None,
@@ -497,81 +421,6 @@ fn register(root: &mut Cursive) {
     }
     login(root);
 }
-
-/*fn api_caller(root: &mut Cursive, endpoint: &str) {
-    let (tx, rx) = mpsc::channel();
-
-    thread::spawn(move || {
-        let client = reqwest::blocking::Client::new();
-        //let response = client.post("https://backend.yap.dragoncave.dev/user")
-        let response = client.post("http://slowwly.robertomurray.co.uk/delay/2000/url/http://www.google.co.uk")
-            .header("Content-Type", "application/json")
-            //.body("{\"username\":\"username-dawdwa\",\"emailAddress\":\"emaildawa@wdaad.aa\",\"password\":\"passworddawdawd\"}")
-            .send().unwrap();
-
-        &tx.send(response);
-    });
-
-
-    loop {
-        match rx.try_recv() {
-            Ok(message) => {
-                notify_popup(root, "worked!", &*format!("token: {}", message.text().unwrap()));
-            },
-            Err(error) => match error {
-                mpsc::TryRecvError::Empty => (),
-                mpsc::TryRecvError::Disconnected => break,
-            }
-        }
-    };
-}*/
-
-/*fn call_api(root: &mut Cursive, action: &str, body: String) {
-
-    let data: &mut Data = root.user_data::<Data>().unwrap();
-
-    let http_client = &data.http_client;
-
-    match action {
-        "add_user" => {
-            match http_client.post("https://backend.yap.dragoncave.dev/user")
-                .header("Content-Type", "application/json")shadow = false
-                .body(body)
-                .send() {
-                Ok(request) => {
-                    data.user_id = request.text()
-                        .unwrap()
-                        .parse::<i64>()
-                        .unwrap();
-
-                    root.set_user_data(&data);
-                    notify_popup(root, "Success!", "Successfully created user")
-                },
-                Err(error) => {
-                    if let Some(status) = error.status() {
-                        notify_popup(root, "Request failed.",
-                                     &*format!("Request failed with code: {}", status))
-                    } else {
-                        notify_popup(root, "Request failed.", "Reason: Unknown");
-                    }
-                },
-            }
-        },
-        "delete_user" => {
-
-        },
-        "get_entries" => {
-
-        },
-        "get_user" => {
-
-        },
-        "modify_user" => {
-
-        },
-        invalid_action => panic!("invalid action: {}", invalid_action)
-    }
-}*/
 
 fn welcome_page(root: &mut Cursive) {
     root.pop_layer();
@@ -646,7 +495,7 @@ fn main() {
                 let http_client = blocking::Client::new();
 
                 root.set_user_data(
-                    User {
+                    GlobalData {
                         token: Some(token_comb.token.clone()),
                         http_client
                     }
